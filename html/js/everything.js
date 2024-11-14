a11b7e815640c40ec87f1ad4a827f9a0353f60ad
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2024-11-13 23:30:56 -0500

    Update everything.js

--- html/js/everything.js
+++ html/js/everything.js
@@ -19,18 +19,17 @@ Last-Modified: <span id="Last-Modified"></span>
 </footer>`);
 document.getElementById("Last-Modified").innerText = document.lastModified;
 document.documentElement.insertAdjacentHTML("afterbegin", `
-<div class="dropdown_element dropdown_base">
-<button class="dropdown_element dropdown_button" onclick="void (
-document.getElementsByClassName('dropdown_item_list')[0].classList.toggle('dropdown_item_list_active'));">
-<div class="dropdown_element dropdown_base_line"></div>
-<div class="dropdown_element dropdown_base_line"></div>
-<div class="dropdown_element dropdown_base_line"></div>
+<div class="dropdown_base">
+<button class="dropdown_button" onclick="dropdown_list.classList.toggle('dropdown_item_list_active')">
+<div class="dropdown_base_line"></div>
+<div class="dropdown_base_line"></div>
+<div class="dropdown_base_line"></div>
 </button>
-<div class="dropdown_element dropdown_item_list">
-<a href="/outdex.html#${location.pathname}" class="dropdown_element dropdown_item">
-${location.pathname.includes("outdex.html")?"Headin' back to Sydney, bruv?":"Explore the <s>Outback</s> outdex"}
+<div class="dropdown_item_list" id="dropdown_list">
+<a href="/outdex.html#${location.pathname}" class="dropdown_item">
+${location.pathname.includes("outdex.html")?"Headin' back to Sydney, bruv?":"Enter the outback steakzone"}
 </a>
-<a class="dropdown_element dropdown_item">
+<a class="dropdown_item">
 More items coming Soon<sup>&#8482;</sup>
 </a>
 </div>
