2084c96847b79af219295bfbd737690ceea79a85
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2024-11-20 17:40:20 -0500

    explain burgmenu better

--- html/js/everything.js
+++ html/js/everything.js
@@ -44,10 +44,16 @@ burgmenu.addEventListener("beforetoggle", ev=> {
 });
 if (location.pathname !== "/"&&!location.pathname.includes("dex")){
   burgmenu.insertAdjacentHTML("beforeend", 
-`<div style="text-align:center;font-family:monospace;">You need to<br>
+`<div style="text-align:center;font-family:monospace;">
+This is where we put the party stuff. You need to<br><br>
+<div>
 <span id="outdextoptext">try the new</span><br>
-<a id="outdexbottomtext" title="Single Page App Mode" href="/outdex.html#${location.pathname}" style="cursor:pointer;">S.P.A.M.</a><br>
-to use this menu.
+<a id="outdexbottomtext" title="Single Page App Mode" href="/outdex.html#${location.pathname}" style="--cursor:pointer;">S.P.A.M.</a><br>
+<input id="roboslider" value="0" type="range" oninput="if (this.value >99) location.assign('/outdex.html#${location.pathname}')" />
+<br />
+<code id="imnotarobot" style="top:-7px;left:4px;">I'm not a robot</code>
+</div><br>
+to bring the party with you.
 </div>`);
 }
 var cog = console.log;
