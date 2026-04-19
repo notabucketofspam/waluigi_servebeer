4b690b32a11af209fd3265461b85c7e25048ce87
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-04-19 11:20:34 -0400

    Update mexec.bat

--- mexec.bat
+++ mexec.bat
@@ -3,4 +3,3 @@ TITLE %~nx1
 SET _comm2=%1
 SET _comm=%_comm2:\=/%
 C:\WINDOWS\system32\cmd.exe /c "set MSYSTEM=MINGW64&& set CHERE_INVOKING=1&& set MSYS2_PATH_TYPE=inherit&& C:\msys64\usr\bin\bash.exe --login -c %_comm%"
-timeout /t 5
