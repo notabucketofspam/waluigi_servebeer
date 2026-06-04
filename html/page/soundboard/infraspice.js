825ace3394bd12fde4b148e923613b4e3e6d7082
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-06-04 02:57:18 -0400

    breakout board

---
+++ html/page/soundboard/infraspice.js
@@ -0,0 +1,156 @@
+// ===================================== a whole lot of topmost ui things ======
+// --------------- this is how we do searches
+var allsounds_datalist = document.getElementById('all-sounds');
+/**@type{string[]} */
+var flatbread = window.board.flatMap(boa => boa.sound.map(sou => boa.name + '/' + sou));
+flatbread.forEach(slice => {
+  var option = document.createElement('option');
+  option.value = slice;
+  allsounds_datalist.appendChild(option);
+});
+var soundsearch = document.getElementById('soundsearch');
+soundsearch.addEventListener('input', ev => {
+  try {
+    if (typeof ev?.target?.value === 'string') {
+      if (ev.target.value === '') {
+        soundsearch.blur();
+        soundsearch.focus();
+      } else if (flatbread.includes(ev.target.value)) {
+        beep(ev.target.value);
+      } else {
+        sessionStorage.setItem('most-recent-search', ev.target.value);
+      }
+    }
+  } catch (err) {}
+});
+function rewind_search() {
+  let mrs_maybe = sessionStorage.getItem('most-recent-search');
+  if (mrs_maybe) {
+    soundsearch.value = mrs_maybe;
+  } else {
+    soundsearch.value = '';
+  }
+  soundsearch.blur();
+  soundsearch.focus();
+}
+function play_from_search() {
+  if (soundsearch.value === '') {
+    soundsearch.blur();
+    soundsearch.focus();
+  } else if (flatbread.includes(soundsearch.value)) {
+    beep(soundsearch.value);
+  }
+}
+// -------- the number of columns for all the groups of buttons
+function setHowManyButtons(ev) {
+  var wideness = ev.target.valueAsNumber;
+  localStorage["sb_width"] = wideness;
+  var body_size = document.body.getBoundingClientRect().width;
+  var max_buttons = Math.trunc(body_size / 160);
+  var wideness_real = Math.min(wideness, max_buttons);
+
+  var div_sounds = document.querySelectorAll('div.sounds');
+  div_sounds.forEach(dale => {
+    dale.style.setProperty('grid-template-columns', `repeat(${wideness_real}, 1fr)`);
+  });
+  var sounds_havers = document.querySelectorAll(':has(>div.sounds)');
+  sounds_havers.forEach(haver => {
+    haver.style.setProperty('max-width', `calc(8.5in + ${(wideness_real - 5) * 160}px)`);
+  });
+}
+var width_control = document.getElementById('width_control');
+width_control.addEventListener('input', setHowManyButtons);
+//var howmany = getComputedStyle(document.querySelector(':root')).getPropertyValue('--how-many-buttons');
+localStorage["sb_width"] ??= getComputedStyle(document.querySelector(':root')).getPropertyValue('--how-many-buttons');
+width_control.setAttribute('value', localStorage["sb_width"]);
+setTimeout(function() {
+  setHowManyButtons({target: width_control});
+});
+
+// ------- that lil button that says "where?"
+document.getElementById('where_help').addEventListener('click', ev => {
+  var greatanswer = document.getElementById('greatanswer');
+  greatanswer.style.left = ev.x + 'px';
+  greatanswer.style.top = ev.y + 'px';
+});
+// ------- some stuff for controlling the local volume
+whichStorage["soundboard_gain"] ??= 0.20;
+somevolume.value = whichStorage["soundboard_gain"];
+setgain(whichStorage["soundboard_gain"]);
+function setgain(g) {
+  gainode.gain.setValueAtTime(g, 0);
+  whichStorage["soundboard_gain"] = g;
+}
+// --------- some more ui things at the top
+whichStorage["soundboard_clockbot_enable"] ??= 0;
+clockbot_enable.checked = Number(whichStorage["soundboard_clockbot_enable"]);
+clockbot_enable.addEventListener('input', (ev) => {
+  whichStorage["soundboard_clockbot_enable"] = Number(ev.target.checked);
+  somevolume.disabled = ev.target.checked;
+});
+somevolume.disabled = clockbot_enable.checked;
+
+whichStorage["soundboard_clockbot_channel_id"] ??= "";
+channel_id.value = whichStorage["soundboard_clockbot_channel_id"];
+
+// =========== put the actual soundboards on the page
+board.forEach(key => make_group(booba, key));
+
+// --------- show the total number of sounds at the top of the page
+setTimeout(function() {
+  var pisces = document.querySelectorAll('div#booba button').length;
+  var toppo = document.querySelector('div#topstuff>h2');
+  toppo.innerText = pisces + 'pcs set';
+});
+
+// --------- where is the boat?
+function dudeWheresMyBoat() {
+  var navboat = document.getElementById('navboat');
+  var shareholders = document.querySelectorAll('summary>h2');
+  shareholders.forEach(man => {
+    navboat.lastElementChild.insertAdjacentHTML('beforeend', `<button class="choking_hazard" onclick="document.getElementById('${man.closest('.sb').id}').scrollIntoView()">${man.innerText.split('\n')[0]}</button>`);
+  });
+  var navboat_shown = localStorage.getItem("soundboard_navboat_captains_style_display");
+  navboat_shown ??= 'none';
+  var captains = document.getElementById("captains");
+  captains.style.display = navboat_shown === 'none' ? 'contents' : 'none';
+  hidemyboat();
+}
+dudeWheresMyBoat();
+function hidemyboat() {
+  var navboat = document.getElementById('navboat');
+  var boat_hider = document.getElementById('boat_hider');
+  var captains = document.getElementById("captains");
+  if (captains.style.display === 'contents') {
+    // we are hiding the boat
+    navboat.classList.remove('navboat_classical');
+    captains.style.display = 'none';
+    boat_hider.innerHTML = '&gt;&gt;&gt;&gt; NAVBOAT';
+  } else {
+    // we are SHOWING the navboat
+    navboat.classList.add('navboat_classical');
+    captains.style.display = 'contents';
+    boat_hider.innerHTML = '&lt;&lt;&lt;&lt; HIDE';
+  }
+  localStorage.setItem('soundboard_navboat_captains_style_display', captains.style.display);
+}
+
+// ======================= this section deals with love ========================
+// =========================== and its repurcussions ===========================
+var kiddos = document.getElementById('love_children');
+kiddos.addEventListener("dragover", ev => ev.preventDefault());
+kiddos.addEventListener("drop", lovelist_drop);
+
+write_lovelist();
+
+setTimeout(setup_open);
+var has_username = false;
+check_online(dat => {
+  if (dat.username) {
+    var username_perchance = document.getElementById("username_perchance")
+    username_perchance.innerHTML = "Logged in as <div><b>" + dat.username + "</b></div>";
+    has_username = true;
+    get_love();
+  }
+});
+
