638f32a2fd1c0f7d42f41ca53391305d035d80b0
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-07-05 13:26:30 -0400

    apparently a tsc fix idk

--- retrochat/src/declarations.d.ts
+++ retrochat/src/declarations.d.ts
@@ -1,11 +1,18 @@
-// Tell TypeScript that ANY import ending in ?raw returns a standard string
+/// <reference types="vite/client" />
+
+declare module '98.css?raw' {
+  const content: string;
+  export default content;
+}
+
+// Keep generic handlers too
 declare module '*?raw' {
   const content: string;
   export default content;
 }
 
-// While we are here, let's add ?inline just in case you ever need it again
 declare module '*?inline' {
   const content: string;
   export default content;
 }
+
